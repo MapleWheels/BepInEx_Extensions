1bc5e8bb4cca1497d0c48a05597d0457b52a8790
Narayan Jainanan <njainanan@hotmail.com>
2020-09-25 13:03:26 -0400

    Fioxed interface return type.

--- BepInEx_Extensions/Configuration/IConfigBindable.cs
+++ BepInEx_Extensions/Configuration/IConfigBindable.cs
@@ -7,7 +7,7 @@ namespace BepInEx.Extensions.Configuration
     {
         ConfigFile Config { get; }
 
-        void Bind(ConfigFile config, ManualLogSource logger, string altSectionName, string altKey, string altDesc, T altDefaultValue);
+        IConfigBindable<T> Bind(ConfigFile config, ManualLogSource logger, string altSectionName, string altKey, string altDesc, T altDefaultValue);
     }
 
     public interface IConfigBindableTypeComparator { }
