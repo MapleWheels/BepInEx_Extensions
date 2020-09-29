2b211e688225c6ff080fc808ec41d5f59272351b
Narayan Jainanan <njainanan@hotmail.com>
2020-09-29 12:44:04 -0400

    Added some more documentation to the example model.

--- ConfigModelTests/Example/ExampleModel.cs
+++ ConfigModelTests/Example/ExampleModel.cs
@@ -14,18 +14,32 @@ namespace ConfigModelTests.Example
     /// </summary>
     public class ExampleModel : ConfigDataModel
     {
+        //Notes: 
+        //You don't need to do every single different way here, it's just to show you the different ways it can be used. 
+        //Pick the way that makes sense to you and just use that one.
+
         public ConfigData<float> ConfigOption1 { get; set; } //must be a property.
-        public ConfigData<float> ConfigOption2 { get; set; } = new ConfigData<float>()  //constructor instantiation style. You can do this or the SetDefaults style below. Your preference.
+        public ConfigData<float> ConfigOption2 { get; set; } = new ConfigData<float>()  //Option A: constructor instantiation style. You can do this or the SetDefaults style below. Your preference.
         {
-            Key = "Config_Variable_Name",   //This will be set to 'configOption2' if not set by you. Defaults to the variable name.
-            DefaultValue = 10f,
-            DescriptionString = "I'm running out of flavor text",
-            AcceptableValues = new AcceptableValueRange<float>(0f, 50f)
-        };
+            Key = "Config_Variable_Name",   //This will be set to 'Config_Variable_Name' in the config file. If not set by you, the default 'Key' name is the variable's name.
+            DefaultValue = 10f,             //Default value.
+            DescriptionString = "I'm running out of flavor text",       //Description
+            AcceptableValues = new AcceptableValueRange<float>(0f, 50f)     //Acceptable values
+        }.PreBindSubscribe((ConfigFile file) =>
+        {
+            //OPTIONAL: You can run anything you need to here BEFORE ConfigFile.Bind() is called.
+        }).PostBindSubscribe((ConfigFile file) =>
+        {
+            //OPTIONAL: You can run anything you need to here AFTER ConfigFile.Bind() is called.
+        });
+
         public ConfigData<int> ConfigOption3 { get; set; }  //Left uninitialized for ExamplePlugin example.
 
         public override void SetDefaults()
         {
+            //Note: This is a helper virtual method that will be called when the model is first instantiated.
+            //You can also do some initialization here, it's up to you if you prefer using the above method or this one.
+
             this.SectionName = "Example Section";   //Define your section name here. 
 
             //you don't need to define everything here; just DefaultValue and DescriptionString are recommended.
